781f1e8f70ec62be83246f5147463b1af1b1571f
mraiko23 <aiearnideas@gmail.com>
2025-11-17 10:15:53 +0700

    Update ping-server.js

--- ping-server.js
+++ ping-server.js
@@ -3,10 +3,10 @@
 const http = require('http');
 
 // Hard-coded targets (fill any of these with a URL to enable pinging)
-const PING_TARGET_1 = 'https://adadadasdasdas.onrender.com';
+const PING_TARGET_1 = 'https://adadadasdasdas-ydbe.onrender.com';
 const PING_TARGET_2 = 'https://one23hostderjit-1-8s5z.onrender.com';
 const PING_TARGET_3 = 'https://freehost228adsahas.onrender.com';
-const PING_TARGET_4 = 'https://yeba228teststockpvb-rvb3.onrender.com';
+const PING_TARGET_4 = 'https://yeba228teststockpvb-2gju.onrender.com';
 const PING_TARGET_5 = 'https://aimodertelegram.onrender.com';
 const PING_TARGET_6 = 'https://aiblack.onrender.com';
 const PING_TARGET_7 = '';
